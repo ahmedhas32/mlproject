12f72954bd00860929da4b0abb8b378de7edeac7
Ahmed Hassan Ahmed Mohammed <58079171+ahmedhas32@users.noreply.github.com>
2024-01-17 09:37:00 +0300

    final model

--- app.py
+++ app.py
@@ -1,28 +1,45 @@
-# ----------------------------------------
-# -- Flask => Intro and Your First Page --
-# ----------------------------------------
-# - Flask Is Micro Framework Built With Python
-# --------------------------------------------
-# - HTML
-# - CSS
-# - JavaScript
-# --------------------------------------------
-import numpy as np 
+from flask import Flask,request,render_template
+import numpy as np
 import pandas as pd
 
-from flask import Flask , render_template , request
-
 from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-skills_app = Flask(__name__)
+application=Flask(__name__)
 
-@skills_app.route("/")
-def homepage():
-  return render_template('homepage.html' , p = "HTML")
+app=application
 
-@skills_app.route("/about")
-def about():
-  return render_template ("about.html")
+## Route for a home page
 
-if __name__ == "__main__":
-  skills_app.run(debug=True, port=8080)
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(port=8080)        
